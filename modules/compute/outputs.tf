f0bfca0883e31723353475c77323deae3672cbe1
PrinceShawtz <129332944+PrinceShawtz@users.noreply.github.com>
2025-09-24 21:09:56 -0400

    Update outputs.tf

--- modules/compute/outputs.tf
+++ modules/compute/outputs.tf
@@ -1,1 +1,10 @@
+output "private_ip" {
+  value = azurerm_linux_virtual_machine.vm.private_ip_address
+}
+output "public_ip" {
+  value = azurerm_public_ip.pip.ip_address
+}
+output "vm_id" {
+  value = azurerm_linux_virtual_machine.vm.id
+}
 
