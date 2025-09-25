c3d8f5d0d586c871fe4c79b64a35c8c1f6ecff40
PrinceShawtz <129332944+PrinceShawtz@users.noreply.github.com>
2025-09-24 21:28:43 -0400

    Update variables.tf

--- modules/nsg/variables.tf
+++ modules/nsg/variables.tf
@@ -1,6 +1,16 @@
-variable "resource_group_name" { type = string }
-variable "location" { type = string }
-variable "name" { type = string }
-variable "security_rules" { type = list(map(any)) }
-variable "tags" { type = map(string) }
+variable "resource_group_name" { 
+  type = string 
+}
+variable "location" { 
+  type = string 
+}
+variable "name" { 
+  type = string 
+}
+variable "security_rules" { 
+  type = list(map(any)) 
+}
+variable "tags" { 
+  type = map(string) 
+}
 
