1a62435b16a9e3ca5a0d085bbe070a0fb61fe4bb
PrinceShawtz <129332944+PrinceShawtz@users.noreply.github.com>
2025-09-24 21:23:33 -0400

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,9 +1,27 @@
-variable "prefix" { type = string default = "tfdemo" }
-variable "location" { type = string default = "eastus" }
-variable "resource_group_name" { type = string default = "tfdemo-rg" }
-variable "admin_username" { type = string default = "azureuser" }
-variable "ssh_pub_key_path" { type = string default = "~/.ssh/id_rsa.pub" }
-variable "vm_size" { type = string default = "Standard_B1s" }
+variable "prefix" { 
+  type = string 
+  default = "tfdemo" 
+}
+variable "location" { 
+  type = string 
+  default = "eastus" 
+}
+variable "resource_group_name" { 
+  type = string 
+  default = "tfdemo-rg" 
+}
+variable "admin_username" { 
+  type = string 
+  default = "azureuser" 
+}
+variable "ssh_pub_key_path" { 
+  type = string 
+  default = "~/.ssh/id_rsa.pub" 
+}
+variable "vm_size" { 
+  type = string 
+  default = "Standard_B1s" 
+}
 variable "tags" {
   type = map(string)
   default = {
