938409bb0208bbc82f5ac92aa4b32105d19fa79e
PrinceShawtz <129332944+PrinceShawtz@users.noreply.github.com>
2025-09-24 21:25:52 -0400

    Update variables.tf

--- modules/compute/variables.tf
+++ modules/compute/variables.tf
@@ -1,9 +1,25 @@
-variable "resource_group_name" { type = string }
-variable "location" { type = string }
-variable "vm_name" { type = string }
-variable "subnet_id" { type = string }
-variable "admin_username" { type = string }
-variable "admin_ssh_public_key" { type = string }
-variable "vm_size" { type = string }
-variable "tags" { type = map(string) }
+variable "resource_group_name" { 
+  type = string 
+}
+variable "location" { 
+  type = string 
+}
+variable "vm_name" { 
+  type = string 
+}
+variable "subnet_id" { 
+  type = string  
+}
+variable "admin_username" { 
+  type = string 
+}
+variable "admin_ssh_public_key" { 
+  type = string 
+}
+variable "vm_size" { 
+  type = string 
+}
+variable "tags" { 
+  type = map(string) 
+}
 
